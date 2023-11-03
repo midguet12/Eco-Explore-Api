7f9752fb44946acb11175f734ae74ff5b7162248
midguet12 <midguet12@hotmail.com>
2023-11-02 19:41:24 -0600

    Update routes.py

--- src/eco_explore_api/routes.py
+++ src/eco_explore_api/routes.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get('/health')
 async def health():
-    return {'status': 'ok ya funciona con watchtower de manera automatica'}
+    return {'status': 'ok ya funciona con watchtower de manera automatica sin errores'}
 
 
 @app.get('/status')
