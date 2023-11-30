3d8a7ce989d14a16e804e0f78c2d4a07be0dc8eb
Zinedinne <miguelzinedinne@gmail.com>
2023-11-30 16:59:35 -0600

    Prueba de token

--- src/eco_explore_api/config.py
+++ src/eco_explore_api/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 
 class EnvVar:
@@ -17,8 +18,24 @@ class EnvVar:
             return alt
 
 
-# Enviroments Variables
 env = EnvVar()
+
+
+class Settings(BaseSettings):
+    auth0_domain: str
+    auth0_api_audience: str
+    auth0_issuer: str
+    auth0_algorithms: str
+    class Config:
+        env_file = ".env"
+
+
+def get_settings():
+    return Settings()
+
+
+# Enviroments Variables
+
 
 DOCUMENT_DB_USER = env.get_variable("DB_USER", "usuario")
 DOCUMENT_DB_PASSWORD = env.get_variable("DB_PASSWORD", "contra")
@@ -27,3 +44,7 @@ USERS_COLLECTION = env.get_variable("USERS_COLLECTION", "usuarios")
 DEFAULT_DATABASE = env.get_variable("DEFAULT_DATABASE", "base de datos")
 DOCUMENT_DB_URL = env.get_variable("DB_URL", "localhost")
 TOKEN_AUTH = env.get_variable("AUTH0_TOKEN")
+AUTH0_DOMAIN = env.get_variable("AUTH0_DOMAIN")
+AUTH0_API_AUDIENCE = env.get_variable("AUTH0_API_AUDIENCE")
+AUTH0_ISSUER = env.get_variable("AUTH0_ISSUER")
+AUTH0_ALGORITHMS = env.get_variable("AUTH0_ALGORITHMS")
--- src/eco_explore_api/routes.py
+++ src/eco_explore_api/routes.py
@@ -1,17 +1,23 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Security
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.security import HTTPBearer
+#from eco_explore_api.auth.utils import VerifyToken
 from datetime import datetime
 from eco_explore_api.schemas.responses import HealthCheckResponse, VersionResponse
 import eco_explore_api.schemas.response_constants as rcodes
 import eco_explore_api.config as cf
 
+
+token_auth_scheme = HTTPBearer()
+
 app = FastAPI()
+#auth = VerifyToken()
 
 
 @app.get("/health", response_model=HealthCheckResponse())
-async def health(saludo):
-    response = HealthCheckResponse(message="Hola {}".format(saludo))
+async def health():
+    response = HealthCheckResponse(message="Hola {}".format(cf.AUTH0_API_AUDIENCE))
     return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(response))
 
 
@@ -24,3 +30,9 @@ async def statue():
             VersionResponse(version=1.0, detail=(HealthCheckResponse(time=time)))
         ),
     )
+
+
+#@app.get("/api/private")
+#def private(auth_result: str = Security(auth.verify)):
+#    """A valid access token is required to access this route"""
+#    return auth_result
