1cff890979ae3c1b3419a482c2f58c688e4572ca
Zinedinne <miguelzinedinne@gmail.com>
2023-12-28 15:24:35 -0600

    Rutas de consulta agregadas y declaraciion de los esquemas de respuesta

--- src/eco_explore_api/routes.py
+++ src/eco_explore_api/routes.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
-from eco_explore_api.schemas.responses import HealthCheckResponse, VersionResponse
+from typing import List
+from eco_explore_api.schemas.responses import (
+    HealthCheckResponse,
+    VersionResponse,
+    EquipoNecesarioResponse,
+    PuntosInteresResponse,
+    ResenaResponse,
+    UsuariosResponse,
+    BitacoraResponse,
+    ExploracionesResponse,
+)
 import eco_explore_api.schemas.response_constants as rcodes
 import eco_explore_api.config as cf
 
@@ -11,7 +21,7 @@ app = FastAPI()
 
 @app.get("/health", response_model=HealthCheckResponse())
 async def health(saludo):
-    response = HealthCheckResponse(message="Hola {}".format(saludo))
+    response = HealthCheckResponse(message="Adios {}".format(saludo))
     return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(response))
 
 
@@ -24,3 +34,51 @@ async def statue():
             VersionResponse(version=1.0, detail=(HealthCheckResponse(time=time)))
         ),
     )
+
+
+@app.get("/usuarios", response_model=List[UsuariosResponse], tags=["Usuarios"])
+async def get_usuarios():
+    usuarios = []
+    return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(usuarios))
+
+
+@app.get(
+    "/puntos_interes/",
+    response_model=List[PuntosInteresResponse],
+    tags=["Puntos de Interés"],
+)
+async def get_puntos_interes():
+    puntos_interes = []
+    return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(puntos_interes))
+
+
+@app.get("/resenas/", response_model=List[ResenaResponse], tags=["Reseñas"])
+async def get_resenas():
+    resenas = []
+    return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(resenas))
+
+
+@app.get(
+    "/equipos_necesarios/",
+    response_model=List[EquipoNecesarioResponse],
+    tags=["Equipos Necesarios"],
+)
+async def get_equipos_necesarios():
+    equipos = []
+    return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(equipos))
+
+
+@app.get("/bitacoras/", response_model=List[BitacoraResponse], tags=["Bitácoras"])
+async def get_bitacoras():
+    bitacoras = []
+    return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(bitacoras))
+
+
+@app.get(
+    "/exploraciones/",
+    response_model=List[ExploracionesResponse],
+    tags=["Exploraciones"],
+)
+async def get_exploraciones():
+    exploraciones = []
+    return JSONResponse(status_code=rcodes.OK, content=jsonable_encoder(exploraciones))
--- src/eco_explore_api/schemas/responses.py
+++ src/eco_explore_api/schemas/responses.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from pydantic_extra_types.phone_numbers import PhoneNumber
 
 
 class HealthCheckResponse(BaseModel):
@@ -11,3 +12,47 @@ class HealthCheckResponse(BaseModel):
 class VersionResponse(BaseModel):
     version: float
     detail: HealthCheckResponse
+
+
+class EquipoNecesarioResponse(BaseModel):
+    Nombre: str
+    Cantidad: int = 0
+
+
+class PuntosInteresResponse(BaseModel):
+    Lon: float
+    Lat: float
+    UrlMedia: Optional[List[str]]
+
+
+class ResenaResponse(BaseModel):
+    Evaluacion: int
+    Comentario: str
+
+
+class UsuariosResponse(BaseModel):
+    Nombre: str
+    ApellidoPaterno: str
+    ApellidoMaterno: str
+    Email: EmailStr
+    UrlImagen: str
+    PerfilPublico: bool
+    Guia: bool
+    Telefono: PhoneNumber
+
+
+class BitacoraResponse(BaseModel):
+    Nombre: str
+    Descripcion: str
+    Dificultad: int
+    EquipoNecesario: Optional[List[EquipoNecesarioResponse]]
+    PuntosInteres: List[PuntosInteresResponse]
+    Comentarios: Optional[List[ResenaResponse]]
+
+
+class ExploracionesResponse(BaseModel):
+    FechaAgendata: datetime
+    Guia: UsuariosResponse
+    Exploradores: List[UsuariosResponse]
+    Ruta: BitacoraResponse
+    Precio: float
